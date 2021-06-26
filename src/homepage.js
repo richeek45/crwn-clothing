b8e7f1286d10dfb23313487fd7756b8fc8b5e57e
Richeek Debnath <richeek45@gmail.com>
2021-06-26 13:11:03 +0530

    Created react app with create-react-app and added files for homepage

---
+++ src/homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './homepage.scss';
+
+const HomePage = () => {
+  return (  
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <h1 className="subtitle">SHOP NOW</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;

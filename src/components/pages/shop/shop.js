b68e2a952e3da64c6c03805fcd994a5b932b1119
Richeek Debnath <richeek45@gmail.com>
2021-07-06 12:03:24 +0530

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new flow for directory/shop

--- src/components/pages/shop/shop.js
+++ src/components/pages/shop/shop.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import { SHOP_DATA } from './shopData'; 
-import CollectionPreview from '../../collection-preview/collection-preview';
+import React from "react";
+import {Route } from 'react-router-dom';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionsOverview from "../../collections-overview/collections-overview"
+import CollectionPage from "../collection/collection";
 
-    this.state = {
-      collections:  SHOP_DATA
-    }
-  }
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+  </div>
+);
 
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}     
-      </div>
-    )
-  }
-}
 
 export default ShopPage;
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from './redux/store'
+import {store, persistor} from './redux/store'
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <PersistGate persistor={persistor}>
         <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>,
